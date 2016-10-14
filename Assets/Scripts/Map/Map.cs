403353b578b84a18c6f02ca726d29fe8f4e4d64e
Dawid Serafin <Dawid Serafin>
2016-10-14 18:55:46 +0200

    Initialized Project Files

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Map : MonoBehaviour {
+
+    [Header("Map Info")]
+    public string tilesetPath;
+    public int mapWidth;
+    public int mapHeight;
+    public float tileSize;
+    public List<Tile>[,] mapTiles;
+        
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Map/Tile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Tile : System.Object {
+
+    public int spriteIndex;
+    public bool isWalkable;
+    public int height;
+
+}
